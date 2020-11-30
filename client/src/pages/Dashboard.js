f378e498095c75ccaddf60dcf9d28bfc0a92878e
lynn7mitchell <lynn7mitchell@gmail.com>
2020-11-30 13:29:45 -0600

    changed login to function instead of class

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import setAuthToken from "../utils/setAuthtoken";
 import axios from "axios";
+// import io from 'socket.io-client';
 
 export class Dashboard extends Component {
   state = {
     user: {}
   };
+  
   componentWillMount() {
     const token = localStorage.getItem("example-app");
 
@@ -22,23 +24,41 @@ export class Dashboard extends Component {
         });
       })
       .catch(err => console.log(err.response));
+
+
+
+      
   }
-  handleLogout = () => {
-    localStorage.removeItem("example-app");
-    this.setState({
-      redirect: true
-    });
-  };
+
+  // componentDidMount(){
+  //   // const socket = io('localhost:3001');
+  //   // socket.on('connection')
+  // }
+  
+  // handleLogout = () => {
+  //   localStorage.removeItem("example-app");
+  //   this.setState({
+  //     redirect: true
+  //   });
+  // };
+  // connectToRoom = (e) =>{
+  //   console.log(e.target.attributes.room.value)
+  // }
   render() {
     return (
       <div>
-        {/* <i className="material-icons account-icon">account_circle</i> */}
+        {/* <i className="material-icons account-icon">account_circle</i>
         <Link to="/">
           <button className="logout-button" onClick={this.handleLogout}>
             Log Out
           </button>
         </Link>
         <h1>Dashboard</h1>
+
+        <Link to={{ pathname: "/chatroom/" + '1234' }}><h3 room={1234} onClick={this.connectToRoom}>John</h3></Link>
+        <Link to={{ pathname: "/chatroom/" + '2345' }}><h3  room={2345}>Paul </h3></Link>
+        <h3  room={3456}>George</h3>
+        <h3  room={4567}>Ringo</h3> */}
       </div>
     );
   }
--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,147 +1,91 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Redirect, Link } from "react-router-dom";
 import axios from "axios";
+import setAuthToken from "../utils/setAuthtoken";
 
-export class SignUp extends Component {
-  state = {
-    firstName: "",
-    lastName: "",
+export default function SignUp() {
+  const [redirect, setRedirect] = useState(false);
+  const [formData, setFormData] = useState({
     email: "",
-    password: ""
+    password: "",
+    confirmPassword: "",
+  });
+  const [errors, setErrors] = useState({});
+
+  // for for field change
+  const onChange = ({ target: { name, value } }) => {
+    setFormData({ ...formData, hasChanged: true, [name]: value });
   };
 
-  onChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  };
-
-  onSubmit = e => {
+  const onSubmit = (e) => {
     e.preventDefault();
 
+    // check for strong password
+    //  8 Characters, 1 uppercase Letter, 1 Lowercase letter 1 Number, and 1 Special Character
+    // /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+    // found regex at https://www.w3resource.com/javascript/form/password-validation.php
+    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+
+    if (passwordRegex.test(formData.password) !== true) {
+      setErrors({
+        password:
+          "Password must be 8 characters and have at least 1 uppercase, 1 lowercase, 1 number, and 1 special character",
+      });
+    }
+
+    if (formData.password !== this.state.confirmPassword) {
+      setErrors({
+        password: "passwords do not match",
+      });
+    }
+
     const newUser = {
-      firstName: this.state.firstName,
-      lastName: this.state.lastName,
-      email: this.state.email,
-      password: this.state.password,
-      katakana: {
-        multipleChoice: {
-          current: 0,
-          highest: 0
-        },
-        fillInTheBlank: {
-          current: 0,
-          highest: 0
-        }
-      },
-      hiragana: {
-        multipleChoice: {
-          current: 0,
-          highest: 0
-        },
-        fillInTheBlank: {
-          current: 0,
-          highest: 0
-        }
-      }
+      username: formData.username,
+      email: formData.email,
+      password: formData.password,
     };
 
     axios
       .post("api/user", newUser)
-      .then(console.log("Thanks for signing up"))
-      .catch(err => console.log(err));
+      .then((res) => {
+        setErrors({
+          errors: "none",
+        });
 
-      alert("Thank you for signing up!")
+        // after use is signed up the app will log them in immediately and redirect to /dashboard
+
+        axios
+          .post("api/user/login", newUser)
+          .then((res) => {
+            // if (res.data.token) {
+            //   const { token } = res.data;
+            //   localStorage.setItem("example-app", token);
+            //   setAuthToken(token);
+            // }
+            // setRedirect(true),
+            //   setErrors({
+            //     errors: {},
+            //   });
+          })
+          .catch((err) => {
+            console.error(err.res.data);
+            setErrors(err.res.data);
+          });
+      })
+      .catch((err) => {
+        console.error(err.res.data);
+        setErrors(err.res.data);
+      });
   };
-
-  render() {
-    const styles = {
-      logo: {
-        display: "block",
-        margin: "0 auto",
-        paddingBottom: "15px",
-        width: "28vw"
-      },
-      main: {
-        textAlign: "center",
-        marginTop: "25vh"
-      }
-    };
-    return (
-      <div style={styles.main}>
-        <Link to={{ pathname: "/" }}>
-          <i className="material-icons back-button">arrow_back</i>
-        </Link>
-        <div>
-          <h1>Sign Up</h1>
-          <div className="row">
-            <form className="col s12" onSubmit={this.onSubmit}>
-                <div className="input-field col s6">
-                  <input
-                    placeholder="First Name"
-                    id="first_name"
-                    type="text"
-                    className="validate"
-                    name="firstName"
-                    onChange={this.onChange}
-                  />
-                  <label htmlFor="last_name">First Name</label>
-                </div>
-                <div className="input-field col s6">
-                  <input
-                    placeholder="Last Name"
-                    id="first_name"
-                    type="text"
-                    className="validate"
-                    name="lastName"
-                    onChange={this.onChange}
-                  />
-                  <label htmlFor="first_name">Last Name</label>
-                </div>
-
-              <div className="row">
-                <div className="input-field col s12">
-                  <input
-                    placeholder="Email"
-                    id="email"
-                    type="email"
-                    className="validate"
-                    name="email"
-                    onChange={this.onChange}
-                  />
-                  <label htmlFor="email">Email</label>
-                </div>
-              </div>
-
-              <div className="row">
-                <div className="input-field col s12">
-                  <input
-                    placeholder="Password"
-                    id="password"
-                    type="password"
-                    className="validate"
-                    name="password"
-                    onChange={this.onChange}
-                  />
-                  <label htmlFor="password">Password</label>
-                </div>
-                <div className="row">
-                  <button
-                    className="btn waves-effect waves-light"
-                    type="submit"
-                    name="action"
-                  >
-                    Submit
-                    <i className="material-icons right">send</i>
-                  </button>
-                </div>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    );
+  if (redirect) {
+    return <Redirect to="/dashboard" />;
   }
+  return( 
+  <div>
+     <Link to={{ pathname: "/" }}>
+        <i className="material-icons back-button">arrow_back</i>
+      </Link>
+      <h3>SIGN UP</h3>
+      
+  </div>);
 }
-
-export default SignUp;
