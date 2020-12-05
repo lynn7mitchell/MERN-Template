36758041d3266128cc0c7a82a57e8872b56ed87c
lynn7mitchell <lynn7mitchell@gmail.com>
2020-12-04 20:09:59 -0600

    added logout button

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
+import { Redirect, Link } from "react-router-dom";
 import setAuthToken from "../utils/setAuthtoken";
 import axios from "axios";
 
 export default function Dashboard() {
 
   const [user, setUser] = useState({})
+  const [redirect, setRedirect] = useState(false);
+
   useEffect(() => {
     // gets the bearer token to validate the user that is logged in
     const token = localStorage.getItem("example-app");
@@ -23,10 +26,21 @@ export default function Dashboard() {
 
   }, []);
 
+  const handleLogout = () => {
+      localStorage.removeItem("example-app");
+      setRedirect(true)
+    };
+
+    if (redirect) {
+      return <Redirect to="/" />;
+    }
   return (
     <div>
       <h1>Dashboard</h1>
       <h2>Welcome {user.firstName + ' ' + user.lastName }</h2>
+      <button className="logout-button" onClick={(e)=>handleLogout(e)}>
+            Log Out
+          </button>
     </div>
   )
 }
